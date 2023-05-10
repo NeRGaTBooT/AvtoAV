a09c9796c261d59dd756419424c999b2ed612531
Oleg Galchinsky <galchinsky.oleg@gmail.com>
2023-05-10 23:33:34 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  let navBtn = document.querySelector('.nav-btn');
+  let nav = document.querySelector('.nav');
+  navBtn.addEventListener('click', function () {
+    nav.classList.toggle('active');
+    navBtn.classList.toggle('active');
+  })
+
+
+  
+});
